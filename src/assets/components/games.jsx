0260225047f7706d21c4efc7112d3b7c2438ea4a
rayan <desmetrayan621@gmail.com>
2025-05-15 11:58:47 +0200

    game.jsx2

--- src/assets/components/games.jsx
+++ src/assets/components/games.jsx
@@ -2,6 +2,14 @@ import React, {useState} from "react";
 
 const choices = ["pierre", "papier", "ciseaux"]; 
 
+const getRandomChoice = () => {
+    if (player === bot) return "Egaliter";
+    if (
+     (player === "pierrre" && === "ciseaux") ||
+     (player === "papier" && === "pierrre") ||
+     (player === "ciseaux" && === "papier") 
+
+    )
 
 
 };
